b7131a45569a7db0fd58fc616174acba58d8e0aa
Alessandro de Almeida <124206367+AlessandrodeAlmeida2@users.noreply.github.com>
2023-10-30 18:59:46 -0300

    Add files via upload

---
+++ pooPerguntaERestposta/Pergunta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaERestposta
+{
+    public class Pergunta
+    {
+        public Pergunta()
+        {
+            this.Texto = "";
+            this.Resposta = "";
+        }
+
+        public Pergunta(string texto, String resposta)
+        {
+            this.Texto = texto;
+            this.Resposta = resposta;
+        }
+
+        public String Texto { get; set; }
+
+        public String Resposta { get; set; }
+    }
+}
---
+++ pooPerguntaERestposta/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaERestposta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PerguntaPlus p = new PerguntaPlus();
+            p.Texto = "Qual o sé o site que possui um ótimo conteúdo sobre programação?";
+            p.Resposta = "www.dfilitto.com.br";
+            p.Dica = "O mantenedor do site é o Danilo Filitto";
+
+            Console.WriteLine("Tente advinhar a pergunta");
+            Console.WriteLine("Pergunta: " + p.Texto);
+            Console.WriteLine("Dica: " + p.Dica);
+            Console.Write("Resposta: ");
+            String resposta = Console.ReadLine();
+
+            if (p.Resposta.ToUpper() == resposta.ToUpper())
+            {
+                Console.WriteLine("Prabens!!! Você acertou!!!");
+            } 
+            else 
+            {
+                Console.WriteLine("Que pena!! Você errou!!!");
+                Console.WriteLine("A resposta correta é: " + p.Resposta);
+            }
+            Console.ReadKey();
+        }
+    }
+}
